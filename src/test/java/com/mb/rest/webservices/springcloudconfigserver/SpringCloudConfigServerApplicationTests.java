232319432f623162efce612c33ba640dbffcf957
boukharoubamehdi <boukharouba.mehdi@gmail.com>
2022-05-13 14:48:30 -0400

    Inital commit

---
+++ src/test/java/com/mb/rest/webservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mb.rest.webservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
